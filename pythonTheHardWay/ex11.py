8aff02385b4019262d16483ec38124da8260735b
davidalex620 <spand3r@gmail.com>
2018-10-29 21:55:03 -0500

    ex11 from pthw

---
+++ pythonTheHardWay/ex11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=' ')
+age = int(input())
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weight?", end=' ')
+weight = input()
+
+dogYears = age * 7
+
+print(f"So, you're {age} old ({dogYears} in dog years), {height} tall and {weight} heavy.")
