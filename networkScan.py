e8c728bc7a0d74ded29ebe097dd7708ccadfdd1f
davidalex620 <spand3r@gmail.com>
2018-12-26 04:35:19 -0600

    error correction

--- networkScan.py
+++ networkScan.py
@@ -11,7 +11,25 @@ def get_argments():
 
 def scan(ip):
 	arp_repuest = scapy.ARP(pdst=ip)
-	broadcast = scapy.Ether(dst)
+	broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+	arp_request_broadcast = broadcast/arp_repuest
+	answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+	clients_list = []
+	for element in answered_list:
+		client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+		clients_list.append(client_dict)
+	return clients_list
+
+def print_result(result_list):
+	print("IP\t\t\tMac Address")
+	print("---------------------------------------")
+	for client in result_list:
+		print(client["ip"] + "\t\t" + client["mac"])
+
+options = get_argments()
+scan_result = scan(options.target)
+print_result(scan_result)
 
 
 
